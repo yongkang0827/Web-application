00e9ec8ae9f1bf40d112d8416db342f1d9730a4f
LimMingYeu <jasonlimmingyeu429@gmail.com>
2021-02-19 07:14:01 +0800

    favourite

--- test2/test2/HDY/ASPX/gallery.aspx.cs
+++ test2/test2/HDY/ASPX/gallery.aspx.cs
@@ -14,11 +14,16 @@ namespace test2.HDY.ASPX
     public partial class gallery : System.Web.UI.Page
     {
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-        String new_id;
+        String favourite_id;
+        string custname, title;
+        string photo;
+        string price;
+        byte[] img;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             con.Open();
+            GenerateId();
             if (!this.IsPostBack)
             {
 
@@ -37,25 +42,52 @@ namespace test2.HDY.ASPX
 
             DataRowView dr = (DataRowView)e.Item.DataItem;
             string imageUrl = "data:image/jpg;base64," + Convert.ToBase64String((byte[])dr["ImgUpload"]);
-            (e.Item.FindControl("Image1") as Image).ImageUrl = imageUrl;
-          
+            (e.Item.FindControl("Image1") as Image).ImageUrl = imageUrl;           
         }
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
-            SqlDataAdapter sdi = new SqlDataAdapter("SELECT * FROM Img WHERE PostId=@PostId", con);
-            //cmd.Parameters.AddWithValue("@id", new_id);
-            //cmd.Parameters.AddWithValue("@photo", bytes);
+            SqlCommand sdi = new SqlCommand("SELECT * FROM Img WHERE PostId=@PostId", con);
+            String id = "PO10";
 
+            sdi.Parameters.AddWithValue("@PostId", id);
+            SqlDataReader dtrProd = sdi.ExecuteReader();
 
+            if (dtrProd.HasRows)
+            {
+                while (dtrProd.Read())
+                {
+                    custname = dtrProd["PostId"].ToString();
+                    title = dtrProd["Title"].ToString();
+                    price = dtrProd["Price"].ToString();
+
+                    img = (byte[])(dtrProd["ImgUpload"]);
+
+                }
+
+                con.Close();
+                con.Open();
+
+                string add = "INSERT INTO Favourite VALUES (@id, @cust, @name, @photo, @price)";
+                SqlCommand favourite = new SqlCommand(add, con);
+
+                favourite.Parameters.AddWithValue("@id", favourite_id);
+                favourite.Parameters.AddWithValue("@cust", custname);
+                favourite.Parameters.AddWithValue("@name", title);
+                favourite.Parameters.AddWithValue("@photo", img);
+                favourite.Parameters.AddWithValue("@price", price);
+                
+                favourite.ExecuteNonQuery();
+                con.Close();
+            }
         }
 
         private void GenerateId()
         {
-            SqlCommand cmdId = new SqlCommand("Select Count(ImgId) FROM ImgFav", con);
+            SqlCommand cmdId = new SqlCommand("Select Count(FavouriteId) FROM Favourite", con);
             int i = Convert.ToInt32(cmdId.ExecuteScalar());
             i++;
-            new_id = "FO" + i.ToString();
+            favourite_id = "FO" + i.ToString();
         }
     }
 }
