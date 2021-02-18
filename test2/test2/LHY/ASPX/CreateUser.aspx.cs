3e46e21b594f6d572b467b3f4a185aaaa19afe17
DESKTOP-K6JSR58\Han Yao <hanyaolim@gmail.com>
2021-02-18 20:59:56 +0800

    login

---
+++ test2/test2/LHY/ASPX/CreateUser.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+namespace test2.LHY.ASPX
+{
+    public partial class CreateUser : System.Web.UI.Page
+    {
+        // create connection -> sampleDB
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        string CustomerID;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            GenerateId();
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            string strAdd = "INSERT INTO Customer VALUES(@ID, @name, @phone,@pass)";
+
+            SqlCommand cmdAdd = new SqlCommand(strAdd, con);
+
+            cmdAdd.Parameters.AddWithValue("@ID", CustomerID);
+
+            cmdAdd.Parameters.AddWithValue("@name", txtUsername.Text);
+
+            cmdAdd.Parameters.AddWithValue("@phone", txtPhone.Text);
+
+            cmdAdd.Parameters.AddWithValue("@pass", txtPassw.Text);
+
+            cmdAdd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void GenerateId()
+        {
+            SqlCommand cmdId = new SqlCommand("Select Count(CustID) FROM Customer", con);
+            int i = Convert.ToInt32(cmdId.ExecuteScalar());
+            i++;
+            CustomerID = "CO" + i.ToString();
+        }
+    }
+}
---
+++ test2/test2/LHY/ASPX/login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace test2.LHY.ASPX
+{
+    public partial class login : System.Web.UI.Page
+    {
+        bool Login = false;
+        // create connection -> sampleDB
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string strAdd = "Select * From Customer Where Username=@id";
+
+            SqlCommand cmdAdd = new SqlCommand(strAdd, con);
+
+            cmdAdd.Parameters.AddWithValue("@ID", txtUsername.Text);
+            SqlDataReader dtrProd = cmdAdd.ExecuteReader();
+
+            if (dtrProd.HasRows)
+            {
+                while (dtrProd.Read())
+                {
+                    string password = txtPassw.Text;
+                    if (password.Equals(dtrProd["Password"].ToString()))
+                    {
+                        Login = true;
+
+                    }
+
+
+
+                }
+            }
+
+            con.Close();
+        }
+    }
+}
