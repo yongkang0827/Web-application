99cde536f4bd7e6b4e3738bb201e0a1d3143f3d7
yong0312 <66929533+yong0312@users.noreply.github.com>
2021-04-01 12:34:29 +0800

    UserControl

--- test2/Master/ASPX/custHome.aspx.cs
+++ test2/Master/ASPX/custHome.aspx.cs
@@ -11,22 +11,7 @@ namespace test2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                slideShow();
-            }
-        }
-        public void slideShow()
-        {
-            Random random = new Random();
-            int i = random.Next(1, 5);
-            Image1.ImageUrl = "../IMG/" + i.ToString() + ".jpg";
 
-        }
-
-        protected void Timer1_Tick(object sender, EventArgs e)
-        {
-            slideShow();
         }
     }
 }
