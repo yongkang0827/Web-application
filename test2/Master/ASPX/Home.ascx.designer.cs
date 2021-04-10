e866ee6ba52751ddd59d7da48a97d3515b6cca8b
LAPTOP-U0MVTNAB\User <chinwk-wm18@student.tarc.edu.my>
2021-04-11 00:10:36 +0800

    update

--- test2/Master/ASPX/Home.ascx.designer.cs
+++ test2/Master/ASPX/Home.ascx.designer.cs
@@ -51,24 +51,6 @@ namespace test2.Master.ASPX
         protected global::System.Web.UI.WebControls.Image Image1;
 
         /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
-
-        /// <summary>
-        /// HyperLink2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
-
-        /// <summary>
         /// lblName control.
         /// </summary>
         /// <remarks>
--- test2/Master/ASPX/artHome.ascx.designer.cs
+++ test2/Master/ASPX/artHome.ascx.designer.cs
@@ -51,15 +51,6 @@ namespace test2.Master.ASPX
         protected global::System.Web.UI.WebControls.Image Image1;
 
         /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
-
-        /// <summary>
         /// lblName1 control.
         /// </summary>
         /// <remarks>
