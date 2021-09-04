5bf17e3872df464bf4fb0ee7442c818abcc1f2c3
kuanxian1 <kuanxian1@gmail.com>
2021-09-04 14:50:50 +0800

    Add to Cart Button can work now

--- PennyJuice/Details.aspx.cs
+++ PennyJuice/Details.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,13 +14,64 @@ namespace PennyJuice
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         // Add To Cart Button
         protected void BtnPurchase_Click(object sender, EventArgs e)
         {
+            var con = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
 
+            string price = String.Empty;
+            string img = String.Empty;
+            string name = String.Empty;
+            string id;
+            id = Session["ArtistList"].ToString();
+
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                string oString = "Select * from Juice where Id=@id";
+                SqlCommand oCmd = new SqlCommand(oString, myConnection);
+                oCmd.Parameters.AddWithValue("@id", id);
+
+                myConnection.Open();
+                using (SqlDataReader oReader = oCmd.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        price = oReader["Price"].ToString();
+                        img = oReader["Image"].ToString();
+                        name = oReader["JuiceName"].ToString();
+                    }
+
+                    myConnection.Close();
+                }
+            }
+
+            //string con = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection gallery = new SqlConnection(con);
+            gallery.Open();
+
+            SqlCommand cmd = gallery.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO [Order] ([Image], [ProductName], [Price], [Quantity]) VALUES(@image, @itemName, @itemPrice, '1')";
+
+            cmd.Parameters.AddWithValue("@image", img);
+            cmd.Parameters.AddWithValue("@itemName", name);
+            cmd.Parameters.AddWithValue("@itemPrice", price);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Response.Write("<script>alert('This item sucessfully add to cart.')</script>");
+            }
+            catch
+            {
+                Response.Write("<script>alert('This item is already in the cart.')</script>");
+            }
+
+            gallery.Close();
+            
         }
     }
 }
